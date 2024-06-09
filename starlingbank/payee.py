a74f4d7714df3f5bba2d0251f1532d57c2707735
tbeachill <tbeachill@proton.me>
2024-06-09 15:38:02 +0100

    added PayeePayment

--- starlingbank/payee.py
+++ starlingbank/payee.py
@@ -3,6 +3,7 @@ from .constants import *
 from .utils import _url
 from requests import get
 from base64 import b64decode
+from datetime import datetime, timedelta
 
 class Payee:
     """Representation of a payee."""
@@ -53,9 +54,9 @@ class Payee:
                     payee_account_uid = account.get("payeeAccountUid")
                     self.payee_accounts[payee_account_uid] = PayeeAccount(
                         self._auth_headers, self._sandbox, self._account_uid,
-                        payee_account_uid
+                        payee_account_uid, self.uid
                     )
-                    self.payee_accounts[payee_account_uid].update(response, self.uid)
+                    self.payee_accounts[payee_account_uid].update(response)
                 return
             
     def get_payee_image(self, filename: str = None) -> str:
@@ -79,7 +80,7 @@ class PayeeAccount:
     
     def __init__(
         self, auth_headers: Dict, sandbox: bool, account_uid: str,
-        payee_account_uid: str
+        payee_account_uid: str, payee_uid: str, payments_since: str = None
     ) -> None:
         self._auth_headers = auth_headers       
         self._sandbox = sandbox
@@ -87,6 +88,7 @@ class PayeeAccount:
 
         # Payee Account Data
         self.account_uid = payee_account_uid
+        self.payee_uid = payee_uid
         self.channel_type = None
         self.description = None
         self.default_account = None
@@ -96,16 +98,78 @@ class PayeeAccount:
         self.bank_identifier_type = None
         self.last_references = [] # type: List[str]
         
-    def update(self, response, payee_uid) -> None:
+        self.payments = {} # type: Dict[str, PayeePayment]
+        self.payments_since = payments_since
+        
+    def update(self, response) -> None:
         for payee in response.get("payees", []):
-            if payee.get("payeeUid") == payee_uid:
+            if payee.get("payeeUid") == self.payee_uid:
                 for account in payee.get("accounts", []):
-                    self.channel_type = account.get("channelType")
-                    self.description = account.get("description")
-                    self.default_account = account.get("defaultAccount")
-                    self.country_code = account.get("countryCode")
-                    self.account_identifier = account.get("accountIdentifier")
-                    self.bank_identifier = account.get("bankIdentifier")
-                    self.bank_identifier_type = account.get("bankIdentifierType")
-                    self.last_references = account.get("lastReferences")
+                    if account.get("payeeAccountUid") == self.account_uid:
+                        self.channel_type = account.get("channelType")
+                        self.description = account.get("description")
+                        self.default_account = account.get("defaultAccount")
+                        self.country_code = account.get("countryCode")
+                        self.account_identifier = account.get("accountIdentifier")
+                        self.bank_identifier = account.get("bankIdentifier")
+                        self.bank_identifier_type = account.get("bankIdentifierType")
+                        self.last_references = account.get("lastReferences")
+                        self._update_payee_payments()
+                        return
+                    
+    def _update_payee_payments(self) -> None:
+        """Get payee payments."""
+        if self.payments_since is None:
+            since_date = datetime.now() - timedelta(days=365)
+            self.payments_since = since_date.strftime("%Y-%m-%d")
+            
+        response = get(
+            _url("/payees/{0}/account/{1}/payments?since={2}".format(
+                self.payee_uid, self.account_uid, self.payments_since), self._sandbox),
+            headers=self._auth_headers
+        )
+        response.raise_for_status()
+        response = response.json()
+        
+        for payment in response.get("payments", []):
+            payment_uid = payment.get("paymentUid")
+            self.payments[payment_uid] = PayeePayment(
+                self._auth_headers, self._sandbox, self._account_uid,
+                self.payee_uid, payment_uid, self.account_uid
+            )
+            self.payments[payment_uid].update(response)
+
+class PayeePayment:
+    """Representation of a payee payment."""
+    
+    def __init__(
+        self, auth_headers: Dict, sandbox: bool, account_uid: str,
+        payee_uid: str, payment_uid: str, payee_account_uid: str
+    ) -> None:
+        self._auth_headers = auth_headers       
+        self._sandbox = sandbox
+        self._account_uid = account_uid
+
+        # Payee Payment Data
+        self.payee_uid = payee_uid
+        self.payment_uid = payment_uid
+        self.payee_account_uid = payee_account_uid
+
+        self.reference = None
+        self.created_at = None
+        self.spending_category = None
+        self.currency = None
+        self.minor_units = None     
+        
+    def update(self, response) -> None:
+        """Update payee payment."""
+        for payment in response.get("payments", []):
+            if payment.get("paymentUid") == self.payment_uid:
+                self.reference = payment.get("reference")
+                self.created_at = payment.get("createdAt")
+                self.spending_category = payment.get("spendingCategory")
+                
+                payment_amount = payment.get("paymentAmount")
+                self.currency = payment_amount.get("currency")
+                self.minor_units = payment_amount.get("minorUnits")
                 return
