2584e92786a6e1dbe3f4b0c2a8edf258b62d8bbf
tbeachill <tbeachill@proton.me>
2024-06-09 17:01:27 +0100

    cleaned up imports

--- starlingbank/__init__.py
+++ starlingbank/__init__.py
@@ -1,10 +1,17 @@
 """Provides limited access to the Starling bank API."""
-from .saving_space import SavingSpace
-from .starling_account import StarlingAccount
-from .spending_space import SpendingSpace
-from .spending_insights import SpendingInsights
+from .address import Address
+from .card import Card
 from .direct_debit import DirectDebit
+from .payee import Payee
+from .round_up import RoundUp
+from .savings_goal import SavingsGoal
+from .spending_insights import SpendingInsights
+from .spending_space import SpendingSpace
 from .standing_order import StandingOrder
+from .starling_account import StarlingAccount
 
-__all__ = ["StarlingAccount", "SavingSpace", "SpendingSpace",
-            "SpendingInsights", "DirectDebit", "StandingOrder"]
+__all__ = [
+    "Address", "Card", "DirectDebit", "Payee",
+    "RoundUp", "SavingsGoal", "SpendingInsights",
+    "SpendingSpace", "StandingOrder", "StarlingAccount"
+]
--- starlingbank/card.py
+++ starlingbank/card.py
@@ -1,4 +1,4 @@
-from typing import List, Dict
+from typing import Dict
 from .constants import *
 from .utils import _url
 from requests import get
